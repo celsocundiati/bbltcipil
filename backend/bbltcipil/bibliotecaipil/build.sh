2482255c8e502fa372392b57906b7363cea94ce3
celsocundiati <cundiaticelso@gmail.com>
2026-07-22 04:22:47 +0100

    Fix build install dependencies before Django commands

--- backend/bbltcipil/bibliotecaipil/build.sh
+++ backend/bbltcipil/bibliotecaipil/build.sh
@@ -11,14 +11,13 @@
 # python manage.py criar_admin
 
 
+
 #!/usr/bin/env bash
 
 set -o errexit
 
-pwd
-ls -la
-
-python manage.py help | grep collectstatic
+pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+
 python manage.py migrate
