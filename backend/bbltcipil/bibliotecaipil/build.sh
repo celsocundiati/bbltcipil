0ffc18431c28aee83f5279a8f3dfd2e7c49e475f
celsocundiati <cundiaticelso@gmail.com>
2026-07-22 04:19:16 +0100

    Debug Render build path

--- backend/bbltcipil/bibliotecaipil/build.sh
+++ backend/bbltcipil/bibliotecaipil/build.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+# set -o errexit
+
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --noinput
+
+# python manage.py migrate
+
+# python manage.py criar_admin
+
+
+#!/usr/bin/env bash
+
 set -o errexit
 
-pip install -r requirements.txt
+pwd
+ls -la
+
+python manage.py help | grep collectstatic
 
 python manage.py collectstatic --noinput
-
 python manage.py migrate
-
-python manage.py criar_admin
